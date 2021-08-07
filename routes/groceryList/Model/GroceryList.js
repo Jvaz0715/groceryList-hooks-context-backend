07316cfcab072d8207699bce08ab5731627be59d
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-07 13:12:00 -0400

    Creates grocery model

---
+++ routes/groceryList/Model/GroceryList.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const grocerySchema = new.mongoose.Schema({
+
+    grocery: {
+        type: String,
+    },
+    isPurchased: {
+        type: Boolean,
+        default: false,
+    },
+    dateAdded: {
+        type: Date,
+        default: () => Date.now(), // default with called function makes sure each grocery gets its on time added... without, it will just be the DATE.NOW of when we create model
+    },
+});
+
+module.exports = mongoose.model("grocery", grocerySchema);
