588e8b32a2e3c3cb15338e0c288315813083eac2
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-07 16:02:49 -0400

    Backend issue, need to find bug

---
+++ routes/groceryList/Controller/groceryListController.js
@@ -0,0 +1,23 @@
+const Grocery = require("../Model/GroceryList");
+
+async function getAllGroceries() {};
+
+async function createGrocery() {};
+
+async function updateGrocery() {};
+
+async function deleteGrocery() {};
+
+async function sortGroceryByPurchased() {};
+
+async function sortGroceryByDate() {};
+
+module.exports = {
+    getAllGroceries,
+    createGrocery,
+    updateGrocery,
+    deleteGrocery,
+    sortGroceryByPurchased,
+    sortGroceryByDate
+}
+
---
+++ routes/groceryList/groceryListRouter.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllGroceries,
+    createGrocery,
+    updateGrocery,
+    deleteGrocery,
+    sortGroceryByPurchased,
+    sortGroceryByDate,
+} = require("./Controller/groceryListController");
+
+/* GET Home Page */
+router.get("/", function(req, res, next) {
+    res.json(true);
+});
+
+router.get("/get-all-groceries", getAllGroceries);
+
+router.post("/create-grocery", createGrocery);
+
+router.put("/update-grocery-by-id/:id", updateGrocery);
+
+router.delete("/delete-grocery-by-id/:id", deleteGrocery);
+
+router.get("/get-groceries-by-purchased", sortGroceryByPurchased);
+
+route.get("/get-groceries-by-sort", sortGroceryByDate);
+
+module.exports = router;
