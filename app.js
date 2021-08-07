edadcd9803142ebc88117b83b9a541acef533bbf
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-07 12:43:15 -0400

    fixes app.js to have server working on 3001, adds necessary code

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// create mongoose server
+mongoose
+  .connect("mongodb://localhost:27017/groceryList-hooksContext-backend",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected for groceryList hooks & context");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-var app = express();
+const groceryListRouter = require("./routes/groceryList/groceryListRouter");
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// is groceryList router
+app.use("/api/v1/groceryList", groceryListRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +43,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json("error", err);
 });
 
 module.exports = app;
